4d0a0a8632a23140ec47465e5ac27e0671166eff
NAKAMURA Shingo <git@itrev.info>
2021-06-11 15:29:44 +0900

    XXXXX

--- autorenew_and_git_commit_push.sh
+++ autorenew_and_git_commit_push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 BASEDIR=/usr/local/www
-F="$BASEDIR/gh_webhook/github_hook.log"
-rm $F
-touch $F
+#F="$BASEDIR/gh_webhook/github_hook.log"
+#rm $F
+#touch $F
 
 cd $BASEDIR/homepage
 sed -e "s/XXXXX/$RANDOM/g" _DUMMY_ > index.html
 /usr/local/bin/nowgitup.sh XXXXX
-clear
 
-tailf $F
+#clear
+#tailf $F
