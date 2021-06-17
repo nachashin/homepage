fc62f5e3fa22eabac9fcfac44dc33ca963b160fb
NAKAMURA Shingo <git@itrev.info>
2021-06-17 11:07:42 +0900

    18827

--- autoghrenew.sh
+++ autoghrenew.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-RAND=$RANDOM
-#sed -e "s/XXXXX/$RAND/g" _DUMMY_ > index.html
-nowgitup.sh $RAND
+if [ -z "$1" ]; then
+    DESCRIPTION=$RANDOM
+else
+    DESCRIPTION="$1"
+fi
+#sed -e "s/XXXXX/$DESCRIPTION/g" _DUMMY_ > index.html
+nowgitup.sh "$DESCRIPTION"
